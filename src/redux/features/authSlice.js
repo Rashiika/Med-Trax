4399dc5ed3ef3000017c35d72a73a9645eb90ae5
Rashiika <rashikaseth2400@gmail.com>
2025-10-26 23:03:44 +0530

    updated domain

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE_URL = "http://13.49.67.184/api";
+const API_BASE_URL = "https://medtrax.me/api";
 axios.defaults.withCredentials = true;
 
 export const roleSelect = createAsyncThunk(
